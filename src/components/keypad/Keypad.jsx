c8da79cf3052b734e85874bb75ec2f6683715d6e
DimaVnuk <vnuchok311994@hotmail.com>
2022-03-02 11:43:27 +0300

    feat: added button from array numberOperand.js

--- src/components/keypad/Keypad.jsx
+++ src/components/keypad/Keypad.jsx
@@ -1,9 +1,22 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import numberOperand from "../../constants/numberOperand";
 export default class Keypad extends Component {
   render() {
-	return (
-	  <div className="keypad-cont">Keypad</div>
-	)
+    return (
+      <div className="keypad-cont">
+        {/* <button className="btn-keypad">C</button>
+		  <button className="btn-keypad">7</button>
+		  <button className="btn-keypad">8</button>
+		  <button className="btn-keypad">9</button>
+		  <button className="btn-keypad">*</button> */}
+        {numberOperand.map((i) => {
+          return (
+            <div className="btn-cont-keypad">
+              <button className="btn-keypad">{i}</button>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }

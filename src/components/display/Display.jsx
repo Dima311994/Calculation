8552d0af1004d17bf2104e3054dd65dbc67e43e4
DimaVnuk <vnuchok311994@hotmail.com>
2022-03-15 22:21:05 +0300

    fix: change props.value in History.jsx

--- src/components/display/Display.jsx
+++ src/components/display/Display.jsx
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
 
 export default class Display extends Component {
-
-
   render() {
     return (
       <div className="display-cont">
         <div className="output-result">
-        {!this.props.isZero ? this.props.defaultValue : '0'}
+          {this.props.value || this.props.memory}
+        </div>
       </div>
-	  </div>
     );
   }
 }
